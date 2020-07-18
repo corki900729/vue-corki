c0835ed83a1e1822efde29e613896a1b0f4a57a9
何亚飞 <yafei_corki@163.com>
2020-07-18 11:57:52 +0800

    first blood

---
+++ src/views/Login/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="login">
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+        <el-row>
+            <el-button round>圆角按钮</el-button>
+            <el-button type="primary" round>主要按钮</el-button>
+            <el-button type="success" round>成功按钮</el-button>
+            <el-button type="info" round>信息按钮</el-button>
+            <el-button type="warning" round>警告按钮</el-button>
+            <el-button type="danger" round>危险按钮</el-button>
+        </el-row>
+    </div>
+</template>
+<script></script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background: #344a5f;
+}
+</style>
